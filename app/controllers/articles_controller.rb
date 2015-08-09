23c45400861591c69ddc6c64aeb5b2c83186d2af
spmno <sunqingpeng@hotmail.com>
2015-08-09 09:48:58 +0800

    init blog

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,40 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_url
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_url
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to articles_url
+  end
+
+  def article_params
+    params.require(:articles).permit(:title, :content)
+  end
+end
