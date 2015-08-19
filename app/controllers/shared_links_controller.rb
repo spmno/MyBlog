fd01324e654a98e02dcbafb231045e68b8c12f3a
spmno <sunqingpeng@hotmail.com>
2015-08-19 17:48:39 +0800

    add http string for link

--- app/controllers/shared_links_controller.rb
+++ app/controllers/shared_links_controller.rb
@@ -1,7 +1,23 @@
 class SharedLinksController < ApplicationController
   def new
+    @link = SharedLink.new
   end
 
   def edit
+    @link = SharedLink.find(params[:id])
+  end
+
+  def create
+    @link = SharedLink.new(link_params)
+    unless @link.link.include?'http'
+      tmp = 'http://' + @link.link
+      @link.link = tmp
+    end
+    @link.save
+    redirect_to root_url
+  end
+
+  def link_params
+    params.require(:shared_link).permit(:name, :link)
   end
 end
